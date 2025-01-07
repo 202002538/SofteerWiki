aa7f5499c9f277a67682689e0ae63967b28c3270
Chaeyeon Lee <cosl1504@naver.com>
2025-01-07 17:33:21 +0900

    feat: W1M3

--- missions/W1/M3/etl_project_gdp_with_sql.py
+++ missions/W1/M3/etl_project_gdp_with_sql.py
@@ -123,6 +123,12 @@ def million_to_billion(gdp):
 ##### wikipidea 페이지가 아닌, IMF 홈페이지에서 직접 데이터를 가져오는 방법은 없을까요? 어떻게 하면 될까요?
 ##### 만약 데이터가 갱신되면 과거의 데이터는 어떻게 되어야 할까요? 과거의 데이터를 조회하는 게 필요하다면 ETL 프로세스를 어떻게 변경해야 할까요?
 
+#1. 윤석님이 보내주신 것처럼 IMF 홈페이지 자체에서 공식적으로 제공하는 api와 json파일을 이용하면 된다
+#2. 데이터베이스도 flyway와 같은 형상 관리 툴이 있다고 한다. 이런것을 이용하면 되지 않을까했는데, ETL 프로세스 자체를 변경해서 과거 데이터를 조회하려면... 새로운 데이터 로드 시에 기존 데이터를 덮어쓰지 않고 저장해둬야되겠다. 지금 보려고 하는 데이터는 GDP니까 GDP 데이터의 예측 시점을 기준으로 테이블에 새로운 데이터를 붙여나가야 할것 같다.
+# 찾아보니 시계열 데이터를 위한 시계열 데이터베이스 같은 경우는 아키텍쳐 디자인이 다르다고 한다. time-stamp기반으로 데이터를 압축, 요약하여 저장한다. 시간 기준으로 아예 파티션을 나눠둔다. 
+# 팀원의 아이디어 - GDP같은 경우는 나라 별로 연도에 따라 큰 변동이 없을 것임, 큰 변동이 나타난 나라에 대한 정보만 저장하면 저장할 데이터량이 줄어들 것이다. 
+
+#----------------------------------------------------------------
 # 추가 요구 사항
 ### 코드를 수정해서 아래 요구사항을 구현하세요.
 ##### 추출한 데이터를 데이터베이스에 저장하세요. 'Countries_by_GDP'라는 테이블명으로 'World_Economies.db'라는 데이터 베이스에 저장되어야 합니다. 해당 테이블은 'Country', 'GDP_USD_billion'라는 어트리뷰트를 반드시 가져야 합니다.
@@ -140,8 +146,7 @@ def connect_and_load(df):
         print("Failed to open database:", e)
 
     cursor = conn.cursor()
-    cursor.execute("DROP TABLE IF EXISTS Countries_by_GDP;") #이미 있던 테이블 제거 후 저장
-    df.to_sql('Countries_by_GDP', conn)
+    df.to_sql('Countries_by_GDP', conn, if_exists='replace') #이미 있던 테이블 제거 후 저장
     print(datetime.datetime.now().strftime('%Y-%B-%d-%H-%M-%S,'), "(Load) 변환된 데이터 Load", file=log_file)       
 
 
@@ -176,7 +181,7 @@ def sendQuery(sql):
         with sqlite3.connect("missions/W1/M3/World_Economies.db") as conn:
             cursor = conn.cursor() #커서 오브젝트 생성
             cursor.execute(sql)
-            conn.commit() #변경사항 저장 - 여기서 해야하나?
+            conn.commit() #변경사항 저장
             return cursor.fetchall() 
         
     except sqlite3.Error as e:
